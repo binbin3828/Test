e0a9625532cac5d299dcc2bb0fea2f79aa88e0dc
MS-HTHXRHTKYKTG\Administrator <510923886@qq.com>
2022-07-24 12:47:47 +0800

    添加map比较函数

---
+++ util/MapUtil.go
@@ -0,0 +1,25 @@
+package util
+
+import "encoding/json"
+
+//比较map[string]interface{}
+func CompareTwoMapInterface(data1 map[string]interface{}, data2 map[string]interface{}) bool {
+	keySlice := make([]string, 0)
+	dataSlice1 := make([]interface{}, 0)
+	dataSlice2 := make([]interface{}, 0)
+	for key, value := range data1 {
+		keySlice = append(keySlice, key)
+		dataSlice1 = append(dataSlice1, value)
+	}
+	for _, key := range keySlice {
+		if data, ok := data2[key]; ok {
+			dataSlice2 = append(dataSlice2, data)
+		} else {
+			return false
+		}
+	}
+	dataStr1, _ := json.Marshal(dataSlice1)
+	dataStr2, _ := json.Marshal(dataSlice2)
+
+	return string(dataStr1) == string(dataStr2)
+}
